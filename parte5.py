f3c7026d71725f0a21695912c56730e276d08fbc
Sharon Sierra <sharonespinoza25@hotmail.com>
2023-08-02 14:32:17 -0600

    Parte 5 hecha

---
+++ parte5.py
@@ -0,0 +1,23 @@
+import os
+import random
+
+class Juego:
+    def __init__(self, mapa, initial_pos, final_pos ):
+        self.mapa = mapa
+        self.initial_pos = initial_pos
+        self.final_pos = final_pos
+        self.resultado = self.procesar_datos()
+
+    def procesar_datos(self):
+        return Juego
+
+resultado = Juego(main_loop["mapa"], main_loop["initial_pos"], main_loop["final_pos"])
+
+
+class JuegoArchivo(Juego):
+    def derivado (self):
+        os.listdir(path)
+        random.choice
+        path_completo = f"{path_a_mapas}/{map1.txt}"
+        cadena.strip()
+        return JuegoArchivo
